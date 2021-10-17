27274b0ee64c06a5d16fb2cff3668ef545885f41
jerry <qin.peng@desty.app>
2021-10-17 21:49:23 +0800

    谷歌地图

--- pages/google-map.vue
+++ pages/google-map.vue
@@ -1,14 +1,13 @@
 
 <template>
   <section class="container">
-    <div class="notice">{{ pacMsg }}</div>
     <div class="pac-box">
       <input 
         id="pac-input" 
         type="text" >
     </div>
     <div id="map"/>
-
+    <div class="notice">{{ pacMsg }}</div>
   </section>
 </template>
 
@@ -41,7 +40,7 @@ export default {
     this.initMap(this.ipLocation)
     this.initMark() // 初始化mark及监听
     this.geocoder = new google.maps.Geocoder() // 根据经纬度获取地址信息
-    this.initInfoWindow()
+    // this.initInfoWindow()
     // 3, 新增地址，根据输入的内容，查询ip，并定位
     this.findPlace('hangzhou')
     // 4, 搜索
@@ -62,31 +61,56 @@ export default {
         },
         mapTypeId: google.maps.MapTypeId.ROADMAP
       })
-      // this.map.addListener('idle', e => {
-      //   console.log('拖拽结束')
-      //   let curPosMiddle = this.map.getCenter()
-      //   if (this.geocoder) {
-      //     this.getAddressByGeocoder(curPosMiddle, {
-      //       rePostion: true
-      //     })
-      //   }
-      // })
+      // 地图平移或缩放事件
+      this.map.addListener('idle', e => {
+        console.log('结束')
+        const bounds = this.map.getBounds()
+        const ne = bounds.getNorthEast() // LatLng of the north-east corner
+        const sw = bounds.getSouthWest() // LatLng of the south-west corder
+        const lat1 = sw.lat(),
+          lat2 = ne.lat(),
+          lng1 = sw.lng(),
+          lng2 = ne.lng()
+        // console.log(lat1, lat2, '=====mapb', lng1, lng2)
+        const markB = this.marker.getPosition()
+        if (!markB) return
+        const lat = markB.lat(),
+          lng = markB.lng()
+        // console.log(lat, lng, '======markB')
+        // console.log(lat1 < lat, lat < lat2, '======markB')
+        const latIn = lat > lat1 && lat < lat2 ? true : false
+        const lngIn = lng > lng1 && lng < lng2 ? true : false
+        // 超出边界才会重现定位
+        if (!latIn || !lngIn) {
+          let curPosMiddle = this.map.getCenter()
+          if (this.geocoder) {
+            this.getAddressByGeocoder(curPosMiddle, {
+              rePostion: true,
+              isCenter: false
+            })
+          }
+        }
+      })
     },
     // 初始化标记和监听
     initMark() {
-      const image =
-        'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+      var locationMarker = {
+        path:
+          'M22 10.5c0 .895-.13 1.76-.35 2.588C20.025 20.723 13.137 28.032 11 28 9.05 28 3.2 21.28.926 14.71.334 13.42 0 11.997 0 10.5c0-.104.013-.206.017-.31C.014 10.117 0 10.04 0 9.967c-.005-.67.065-1.112.194-1.398C1.144 3.692 5.617 0 11 0c5.416 0 9.906 3.74 10.82 8.657.112.29.18.696.18 1.31 0 .083-.013.167-.015.25.003.095.015.188.015.283zM11 5.833c-2.705 0-4.898 2.09-4.898 4.667S8.295 15.167 11 15.167s4.898-2.09 4.898-4.667c0-2.578-2.193-4.667-4.898-4.667z',
+        fillColor: 'blue',
+        fillOpacity: 1,
+        strokeColor: '#E84643'
+      }
       this.marker = new google.maps.Marker({
         map: this.map,
         draggable: true,
-        icon: image,
-        title: 'Material Icon Font Marker'
+        icon: locationMarker
       })
-      // this.map.setCenter(localPlace)
       this.marker.addListener('dragend', event => {
-        console.log(event.latLng.lng(), event.latLng.lat(), '======event')
+        // console.log(event.latLng.lng(), event.latLng.lat(), '======event')
         this.getAddressByGeocoder(event.latLng, {
-          rePostion: false
+          rePostion: false,
+          isCenter: true
         })
       })
     },
@@ -120,15 +144,16 @@ export default {
         input,
         options
       )
-      console.log(autocomplete, '========autocompleteMsg')
+      // console.log(autocomplete, '========autocompleteMsg')
       autocomplete.addListener('place_changed', () => {
         const place = autocomplete.getPlace()
         this.getAddressByGeocoder(place.geometry.location, {
-          rePostion: true
+          rePostion: true,
+          isCenter: true
         })
       })
     },
-    // 信息面板
+    // 信息面板---需求变更，废弃
     initInfoWindow() {
       const infoWindow = new google.maps.InfoWindow()
       infoWindow.close()
@@ -171,16 +196,16 @@ export default {
       this.service = new google.maps.places.PlacesService(this.map)
       this.service.findPlaceFromQuery(request, (results, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-          console.log(results, '=======results==findPlaceFromQuery')
+          // console.log(results, '=======results==findPlaceFromQuery')
           this.ipLocation = results[0].geometry.location
-          console.log(this.ipLocation, '==========ipLocation')
+          // console.log(this.ipLocation, '==========ipLocation')
           this.marker?.setPosition(this.ipLocation.latLng)
         }
       })
     },
     // 得到选择列表
     async pasrseAddress(results, status, options) {
-      console.log(results, '=======results')
+      // console.log(results, '=======results')
       if (status === 'OK') {
         this.marker.setVisible(false)
         results = results.sort(
@@ -194,20 +219,23 @@ export default {
           .filter(item => !!item)
           .join(',')
 
-        console.log(getAddressList, '=======getAddressList')
+        // console.log(getAddressList, '=======getAddressList')
         const location = results[1].geometry.location
         const postcode = this.addressResult(
           results[1].address_components,
           'postal_code'
         )
-        console.log(location.lng(), location.lat(), '========locationget')
+        // console.log(location.lng(), location.lat(), '========locationget')
         // 是否需要重新定位---移动mark不需要重新定位
         if (options.rePostion === true) {
           console.log('into is reposition')
           this.marker?.setPosition(location)
         }
         this.marker.setVisible(true)
-        this.map.panTo(this.marker.getPosition())
+        if (options.isCenter === true) {
+          console.log('into is center')
+          this.map.panTo(this.marker.getPosition())
+        }
         // await this.getAddressLocation(
         //   location.lat().toString(),
         //   location.lng().toString()
@@ -223,7 +251,7 @@ export default {
           // addressCity : state.response.addressCity,
           getAddressFlag: true
         }
-        console.log(stateT, '======state')
+        // console.log(stateT, '======state')
         this.pacMsg = stateT
       }
     },
@@ -261,9 +289,6 @@ export default {
 </script>
 
 <style>
-#map {
-  height: 100%;
-}
 /* Optional: Makes the sample page fill the window. */
 html,
 body {
@@ -272,13 +297,10 @@ body {
   padding: 0;
 }
 .container {
-  display: flex;
-  flex-direction: column;
   padding: 30px;
   box-sizing: border-box;
-  position: relative;
   width: 100%;
-  height: 600px;
+  height: 100vh;
 }
 .notice {
   min-height: 40px;
@@ -288,23 +310,9 @@ body {
   height: auto;
 }
 #map {
-  flex-grow: 1;
-}
-.search {
-  position: absolute;
-  top: 120px;
-  border: 1px solid #d5d5d5;
-  left: 45px;
-  right: 45px;
-  background: cornflowerblue;
-}
-.search input {
-  height: 40px;
-  width: 100%;
-}
-.search li {
-  padding: 5px;
-  color: #666666;
+  /* flex-grow: 1; */
+  height: 400px;
+  margin-bottom: 10px;
 }
 .pac-box input {
   height: 40px;
@@ -326,11 +334,5 @@ body {
 }
 .gm-ui-hover-effect {
   display: none !important;
-}
-#content {
-  padding: 15px;
-  background: blue;
-  color: #ffffff;
-  display: flex;
 }
 </style>
